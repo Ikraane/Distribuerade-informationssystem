be88e65cea93aabdc3d5b5d3f1d73cb8e9707625
Ikran Dahir <91944875+Ikraane@users.noreply.github.com>
2022-10-11 22:15:38 +0200

    Update ShoppingCartServlet.java

--- WebShop/src/com/test/servlets/ShoppingCartServlet.java
+++ WebShop/src/com/test/servlets/ShoppingCartServlet.java
@@ -1,5 +1,22 @@
 package com.test.servlets;
 
-public class ShoppingCartServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
+@WebServlet(name = "ShoppingCartServlet", urlPatterns = "/shoppingCart")
+public class ShoppingCartServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ArrayList<String> cart = (ArrayList<String>) req.getSession().getAttribute("cart");
+
+        req.setAttribute("carts", cart);
+        getServletContext().getRequestDispatcher("/html/shoppingCart.jsp").forward(req, resp);
+    }
 }
