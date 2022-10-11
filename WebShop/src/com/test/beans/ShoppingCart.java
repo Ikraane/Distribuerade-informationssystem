48f32b09851af821c938361888e14ad1eadae6ce
Ikran Dahir <91944875+Ikraane@users.noreply.github.com>
2022-10-11 22:15:04 +0200

    Update ShoppingCart.java

--- WebShop/src/com/test/beans/ShoppingCart.java
+++ WebShop/src/com/test/beans/ShoppingCart.java
@@ -1,5 +1,32 @@
 package com.test.beans;
 
-public class ShoppingCart {
+import com.test.dao.DBUser;
+import com.test.dao.ProductDB;
 
+import java.util.List;
+
+public class ShoppingCart {
+    private DBUser user;
+    private List<ProductDB> productsInCart;
+
+    public ShoppingCart(DBUser user, List<ProductDB> productsInCart) {
+        this.user = user;
+        this.productsInCart = productsInCart;
+    }
+
+    public DBUser getUser() {
+        return user;
+    }
+
+    public void setUser(DBUser user) {
+        this.user = user;
+    }
+
+    public List<ProductDB> getProductsInCart() {
+        return productsInCart;
+    }
+
+    public void setProductsInCart(List<ProductDB> productsInCart) {
+        this.productsInCart = productsInCart;
+    }
 }
